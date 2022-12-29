8d6b106ff1f60b2eca79977eda09d205ffd86037
kyutae1994 <k305059ute@gmail.com>
2022-12-29 21:51:57 +0900

    조인 공부

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         // 어플리케이션 로딩 시점에 딱 하나만 만들어야됨
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("kim");
 
         // 예를 들어 행위를 하고 나가는 경우 db 커넥션을 얻어서 쿼리 날리고 종료할때마다
         // 데이터베이스 커넥션 하나 받았다고 생각하면 된다.
@@ -20,24 +20,23 @@ public class Main {
         // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
         try {
 
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("team");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
-                    .getResultList();
-
-            for (Member member1 : result) {
-                System.out.println(member1);
-            }
+            List<Member> result = em.createQuery("select m from Member m left join Team t on m.username = t.name", Member.class)
+                .getResultList();
 
             tx.commit();
         } catch (Exception e) {
